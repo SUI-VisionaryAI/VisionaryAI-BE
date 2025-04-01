8b316c24751a04f80390b003dfa731c9d2c78334
Hiro <huyp26102000@gmail.com>
2025-04-01 12:56:19 +0700

    fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
